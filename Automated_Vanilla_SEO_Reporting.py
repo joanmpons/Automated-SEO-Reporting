37666b56689ebc30e168a247bca8540ea8b73fe2
joanmpons <148986309+joanmpons@users.noreply.github.com>
2024-02-13 15:28:45 +0100

    Update Automated_Vanilla_SEO_Reporting.py

--- Automated_Vanilla_SEO_Reporting.py
+++ Automated_Vanilla_SEO_Reporting.py
@@ -20,60 +20,17 @@ from parsel import Selector
 
 class google_scraper:
     cookies = {
-    'CONSENT': 'YES+',
-    'SID': 'Zwjkj5-9MAGqWLAvuB9t0IrjS5yOMWXod8T9pBKO_240vxGJUWBbkdgwO0HZUJUK2Izo7Q.',
-    '__Secure-1PSID': 'Zwjkj5-9MAGqWLAvuB9t0IrjS5yOMWXod8T9pBKO_240vxGJ2q6TJcle-f9-RLQhAkqwSA.',
-    '__Secure-3PSID': 'Zwjkj5-9MAGqWLAvuB9t0IrjS5yOMWXod8T9pBKO_240vxGJ8ND2oH_vGLPuC9wSavMnew.',
-    'HSID': 'ANAdZFmGocDhxrkiE',
-    'SSID': 'A92EIlMBd2nKLaq0j',
-    'APISID': '6hMZHwrO-HLmkibI/AfEZlEgqqGKpaaAgx',
-    'SAPISID': '3m80mJYy7fj3-oTk/AwhGxd5nNi7faYchZ',
-    '__Secure-1PAPISID': '3m80mJYy7fj3-oTk/AwhGxd5nNi7faYchZ',
-    '__Secure-3PAPISID': '3m80mJYy7fj3-oTk/AwhGxd5nNi7faYchZ',
-    '1P_JAR': '2023-8-8-18',
-    'AEC': 'Ad49MVGLWdHk7wU_E-4w0qT_RSbo0_Cw_OCP-SfiNvlNGOYH4m5Zlb0UNQ',
-    'SOCS': 'CAISNQgCEitib3FfaWRlbnRpdHlmcm9udGVuZHVpc2VydmVyXzIwMjMwODE1LjA2X3AyGgJlbiAEGgYIgIyApwY',
-    'DV': 'A5rYlOV_NhYoEBgRRJkc1dLhdnAqodiJV5getGLf6QEAAAA',
-    'SIDCC': 'APoG2W8bViUAOtSwaywNcNFDXQN7WcJzcdFIPcCl8Sh-CCCGicQDMx3ammwVsOlGT-bv7r9Xogk',
-    '__Secure-1PSIDCC': 'APoG2W_HzWlpvvDNhQjqXs3OX5ReZd6X_lNvT4G5mOAK474-qPya8Y7wJRW3qp_e--lVAtwVOsQ',
-    '__Secure-3PSIDCC': 'APoG2W98vnGndsj51ptuDzmFbb7VG5hsmyFaFO3e1tc6cpVG5SyXpr85NuvpDAb8VVj3BFBohQ',
-    'NID': '511=RkZqtocnL5Gb3U9sIO8j7FNJWF5KntvLfBGZRtCIxP_vTloIuRriAkJYpWj4FAnwZqnPr1Gi3Mny0VLBJiEZ5T0TmWiOhJMgRmjwuIpJJ3D6OCq8BLU0OWMbjg9BEwwyROIpNBSgSn8WRbalLxAcR9L92A09a1J09NXlhvyMCi6KjTQH6LACbKFEX29MwUY3F1B8kXgLD220pGXMioYLqXFNw4Tvb3omVSydmrWmq4n-HpODadezl4Fd5WFtMENvOK-MrzzEdo4',
+        #Add cookies from browser
     }
 
     headers = {
-    'authority': 'www.google.com',
-    'accept': '*/*',
-    'accept-language': 'es-ES,es;q=0.9,en;q=0.8,fr;q=0.7,ca;q=0.6',
-    'referer': 'https://www.google.com/',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-arch': '"x86"',
-    'sec-ch-ua-bitness': '"64"',
-    'sec-ch-ua-full-version': '"115.0.5790.114"',
-    'sec-ch-ua-full-version-list': '"Not/A)Brand";v="99.0.0.0", "Google Chrome";v="115.0.5790.114", "Chromium";v="115.0.5790.114"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-ch-ua-platform-version': '"10.15.7"',
-    'sec-ch-ua-wow64': '?0',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-    'x-client-data': 'CI22yQEIprbJAQipncoBCKLdygEIlqHLAQiFk80BCIagzQEIk6vNAQjEsc0BCN20zQEI3L3NAQi7vs0BCODEzQEI78TNAQirxc0BCN3FzQEI9cXNAQiWyM0BGPamzQE=',
+        #Add headers from browser
     } 
     
     params = {'q': '',
-              'cp': 0,
-              'client': 'desktop-gws-wiz-on-focus-serp',
-              'xssi': 't',
-              'gs_pcrt': 3,
-              'hl': 'en-ES',
-              'authuser': 0,
               'pq': '',
-              'psi': 'GdDgZPrGB52Li-gP9tum-A8.1692454938338',
-              'dpr': 2,
-              'ofp': 'EAEyewoMCgp1bm8gb25saW5lChIKEHVubyBvcmdhbml6YXRpb24KCwoJdW5vIGNhcmRzCgsKCXVubyBydWxlcwoNCgt1bm8gd2Vic2l0ZQoQCg51bm8gdmlkZW8gZ2FtZQoKCgh1bm8gZnJlZQoOCgx1bm8gZG93bmxvYWQQRzJhChgKFklzIFVubyBhIFNwYW5pc2ggZ2FtZT8KJAoiSG93IGRvIHlvdSBwbGF5IHRoZSBjYXJkIGdhbWUgVW5vPwocChpXaGF0IGlzIHRoZSA3IHJ1bGUgaW4gVW5vPxDkAg',
               'start':''
+              #Add remaining parameters from browser
               }
     base_url = 'https://www.google.com/search'
     
